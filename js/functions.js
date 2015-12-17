0d1f9fb27dfd8ee98160214f711469df24e7d8a3
jambik <jambik@gmail.com>
2015-12-17 12:00:42 +0300

    - first commit

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".contacts a").fancybox({
+		'hideOnContentClick' : true,
+		'padding' : 2,
+		'overlayOpacity' : 0.5,
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	400,
+		'speedOut'	:	200
+	});
+});
